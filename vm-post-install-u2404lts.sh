ae2f1712f47b29b26f1b3efc3532fcc220d8ea5a
Nazmul Ahasan Shawn <nzahasan@gmail.com>
2025-04-22 09:28:02 +0900

    Update vm-post-install-u2404lts.sh

--- vm-post-install-u2404lts.sh
+++ vm-post-install-u2404lts.sh
@@ -1,14 +1,17 @@
 /#!usr/bin/env bash
 
-echo "Install required guest-agent/ceph/network package"
-echo "-------------------------------------------------"
+echo "1) Install required guest-agent/ceph/network package"
+echo "----------------------------------------------------"
 
 apt update && \
-apt install qemu-guest-agent ceph-common ceph-fuse net-tools iputils-ping
+apt install qemu-guest-agent ceph-common ceph-fuse net-tools iputils-ping rsyslog
 
-echo "Create cfswrite group with gid=7890"
-echo "-----------------------------------"
+echo "2) Create cfswrite group with gid=7890"
+echo "--------------------------------------"
 
 groupadd -g 7890 cfswrite
+
+echo "2) config rsyslog to send log to 10.21.26.210"
+echo "---------------------------------------------"
 
 # end
