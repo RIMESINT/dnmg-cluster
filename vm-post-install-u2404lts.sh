bd2f4284b9e85881ec599e55f2b70b5297cc2411
Nazmul Ahasan Shawn <nzahasan@gmail.com>
2025-05-05 08:52:26 +0600

    Update vm-post-install-u2404lts.sh

--- vm-post-install-u2404lts.sh
+++ vm-post-install-u2404lts.sh
@@ -4,14 +4,22 @@ echo "1) Install required guest-agent/ceph/network package"
 echo "----------------------------------------------------"
 
 apt update && \
-apt install qemu-guest-agent ceph-common ceph-fuse net-tools iputils-ping rsyslog
+apt install -y qemu-guest-agent ceph-common ceph-fuse net-tools iputils-ping rsyslog cloud-init
 
 echo "2) Create cfswrite group with gid=7890"
 echo "--------------------------------------"
 
 groupadd -g 7890 cfswrite
 
-echo "3) config rsyslog to send log to 10.21.26.210"
-echo "---------------------------------------------"
+echo "3) configure rsyslog to send log to 10.21.26.210"
+echo "------------------------------------------------"
+
+printf "\n\n\n# ### Log Forwarding Added ###\n*.* @10.21.26.210:513\n\n#\n" >> /etc/rsyslog.conf
+
+echo "4) configure cloudinit"
+echo "----------------------"
+
+systemctl enable cloud-init && cloud-init clean
+
 
 # end
